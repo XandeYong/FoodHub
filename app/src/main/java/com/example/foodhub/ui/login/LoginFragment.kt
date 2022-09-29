9c4b3b2d02691b92794af92f6fb90bc59690a85d
darrenkaw29 <kawkz-wm19@student.tarc.edu.my>
2022-09-29 14:49:02 +0800

    - Update Fragment

--- app/src/main/java/com/example/foodhub/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/foodhub/ui/login/LoginFragment.kt
@@ -1,20 +1,29 @@
 package com.example.foodhub.ui.login
 
-import android.content.Intent
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.foodhub.databinding.FragmentLoginBinding
 import com.example.foodhub.ui.main.MainFragmentDirections
-import com.example.foodhub.ui.register.RegisterFragment
-import com.example.foodhub.ui.register.RegisterFragmentDirections
-
+import org.json.JSONObject
+import kotlin.collections.HashMap
+import kotlin.collections.Map
+import kotlin.collections.MutableMap
+import kotlin.collections.set
 
 class LoginFragment : Fragment() {
 
@@ -22,12 +31,20 @@ class LoginFragment : Fragment() {
         fun newInstance() = LoginFragment()
     }
 
+    private val URL: String = "http://10.0.2.2/foodhub_server/account.php"
+
     private lateinit var binding: FragmentLoginBinding
     private lateinit var viewModel: LoginViewModel
-    private lateinit var shared : SharedPreferences
-    private var loginAttempt : Int = 0
-
     private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+    var email: String = ""
+    var password: String = ""
+
+    var dbId: String = ""
+    var dbName: String = ""
+    var dbPassword: String = ""
+    var dbEmail: String = ""
+    var dbAccountType: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,38 +52,32 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater)
 
+
+
         binding.button1.setOnClickListener {
 
-//            if(binding.txtEmail.text.toString().isNullOrEmpty() || binding.txtPasswordLogin.text.toString().isNullOrEmpty()){
-//                if(binding.txtEmail.text.toString().isNullOrEmpty()){
-//                    binding.txtEmail.error = "Cannot Be Empty"
-//                }
-//                if(binding.txtPasswordLogin.text.toString().isNullOrEmpty()){
-//                    binding.txtPasswordLogin.error = "Cannot Be Empty"
-//                }
-//            }
-//             if (!binding.txtEmail.text.toString().trim().matches(emailPattern.toRegex())) {
-//                binding.txtEmail.error = "Incorrect Email"
-//            }
-//            else {
-//                if(binding.txtPasswordLogin.text.toString() != "12" && loginAttempt < 5){
-//                    // all the things must get from database
-//                    ++loginAttempt
-//                    binding.txtPasswordLogin.error = "Password Not Correct"
-//                    Toast.makeText(requireContext(), "Invalid Log in Credential. Remaining Attempt : $loginAttempt" , Toast.LENGTH_LONG).show()
-//                }else if (binding.txtPasswordLogin.text.toString() == "12" && loginAttempt < 5) {
-//                    // here need share preferences
-//                    Toast.makeText(context, "Welcome back!" , Toast.LENGTH_LONG).show()
-//                    findNavController().navigate(MainFragmentDirections.actionMainFragmentToMainActivityLogged())
-//                }
-//                else {
-//                    binding.txtPasswordLogin.error = "Password Not Correct"
-//                    Toast.makeText(this.requireContext(), "Used Out attempt Limit, Pls try Again Later" , Toast.LENGTH_LONG).show()
-//                }
-//            }
-            findNavController().navigate(MainFragmentDirections.actionMainFragmentSelf())
 
+
+            email = binding.txtEmail.text.toString()
+            password = binding.txtPasswordLogin.text.toString()
+
+
+            if (email.isNullOrEmpty() || password.isNullOrEmpty()
+            ) {
+                if (email.isNullOrEmpty()) {
+                    binding.txtEmail.error = "Cannot Be Empty"
+                }
+                if (password.isNullOrEmpty()) {
+                    binding.txtPasswordLogin.error = "Cannot Be Empty"
+                }
+            }
+            if (!email.trim().matches(emailPattern.toRegex())) {
+                binding.txtEmail.error = "Incorrect Email"
+            } else {
+                LoginCheck()
+            }
         }
+
         binding.txtSignUp.setOnClickListener() {
             findNavController().navigate(MainFragmentDirections.actionMainFragmentToRegisterFragment()) // not working
 
@@ -75,9 +86,78 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    fun LoginCheck(): Boolean {
+        var status = false;
+
+        val stringRequest: StringRequest = object : StringRequest(
+            Request.Method.POST, URL,
+            Response.Listener { response ->
+                val jsonResponse = JSONObject(response)
+                val myobject: JSONObject
+                val myObjAsString = jsonResponse.getJSONArray("data")
+
+                if (myObjAsString.length() > 0) {
+                    myobject = myObjAsString.getJSONObject(0)
+                    dbId = myobject.get("account_id").toString()
+                    dbName = myobject.get("name").toString()
+                    dbEmail = myobject.get("email").toString()
+                    dbPassword = myobject.get("password").toString()
+                    dbAccountType = myobject.get("account_type").toString()
+                }
+                if (password == dbPassword) {
+                    val preferences =
+                        activity?.getSharedPreferences("login_S", Context.MODE_PRIVATE)
+                    val editor = preferences?.edit()
+
+                    editor?.putString("accountID", dbId)
+                    editor?.putString("name", dbName)
+                    editor?.putString("password", dbPassword)
+                    editor?.putString("accountType", dbAccountType)
+                    editor?.apply()
+                    editor?.commit()
+
+                    Toast.makeText(context, "Welcome back!", Toast.LENGTH_LONG).show()
+//                    findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToNewsFragment2()) // unable to navigate
+                } else {
+                    binding.txtPasswordLogin.error = "Password Not Correct"
+                    Toast.makeText(requireContext(), "Invalid Password.", Toast.LENGTH_LONG).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(
+                    requireContext(),
+                    error.toString().trim { it <= ' ' },
+                    Toast.LENGTH_SHORT
+                ).show()
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String>? {
+
+                val data: MutableMap<String, String> = HashMap()
+                data["Content-Type"] = "application/x-www-form-urlencoded"
+                data["request"] = "login"
+                data["email"] = email
+                data["password"] = password
+                return data
+            }
+        }
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        requestQueue.add(stringRequest)
+        Log.i("BeforeLast", status.toString())
+        return status
+    }
+
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+//        viewModel.response.observe(viewLifecycleOwner, Observer {
+//            if (it.toString().isNotEmpty()) {
+//                Log.i("Yes123", it.toString())
+//            }
+//        }
+//        )
 
     }
 
